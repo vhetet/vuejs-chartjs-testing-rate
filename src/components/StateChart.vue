e5bdafd2fe68eb0cc7971d386602012df0d7d8a1
vhetet <vh1030478@gmail.com>
2020-04-19 10:16:25 -0500

    start adding state selection dropdown

--- src/components/StateChart.vue
+++ src/components/StateChart.vue
@@ -1,12 +1,19 @@
 <template>
+
   <div class="hello">
+    <select v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <span>Selected: {{ selected }}</span>
     <line-chart :chart-data="datacollection" :options="options"></line-chart>
   </div>
 </template>
 
 <script>
 import LineChart from './LineChart.js'
-import data from '../assets/data/new-york_covid_test_daily_positive_rate.json'
 
   export default {
     components: {
@@ -16,30 +23,35 @@ import data from '../assets/data/new-york_covid_test_daily_positive_rate.json'
       return {
         datacollection: {},
         options: {},
-        data: data
+        data: [],
+        stateList: require('../assets/data/state_list.json')
       }
     },
     mounted () {
+      this.getData('new-york'),
       this.fillData()
     },
     methods: {
+      getData(state) {
+        this.data = require(`../assets/data/${state}_covid_test_daily_positive_rate.json`)
+      },
       fillData () {
         this.datacollection = {
-          labels: data.slice(12).map(x => x.date),
+          labels: this.data.slice(12).map(x => x.date),
           datasets: [
             {
               label: 'Illinois covid testing daily positive rate',
               yAxisID: 'a',
               borderColor: 'rgba(255, 111, 111)',
               backgroundColor: 'rgba(255, 111, 111, 0.3)',
-              data: data.slice(12).map(x => x.dailyPositiveCasePercentage)
+              data: this.data.slice(12).map(x => x.dailyPositiveCasePercentage)
             },
             {
-              label: 'Number of test / 100',
+              label: 'Number of test',
               yAxisID: 'b',
               borderColor: 'rgba(100, 111, 255)',
               backgroundColor: 'rgba(100, 111, 255, 0.3)',
-              data: data.slice(12).map(x => x.dailyTest)
+              data: this.data.slice(12).map(x => x.dailyTest)
             }
           ]
         },
