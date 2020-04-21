0a4db4fff0e8cf179c93dc22e1bc493f9f05527f
vhetet <vh1030478@gmail.com>
2020-04-21 18:42:06 -0500

    rename chart component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <StateChart msg="Welcome to Your Vue.js App"/>
+    <PositivityRateChart msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import StateChart from './components/StateChart.vue'
+import PositivityRateChart from './components/PositivityRateChart.vue'
 
 export default {
   name: 'App',
   components: {
-    StateChart
+    PositivityRateChart
   }
 }
 </script>
