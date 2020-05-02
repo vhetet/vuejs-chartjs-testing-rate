cd0cf906bd5f3b1bba0e292cc7da24802ce3c4c5
vhetet <vh1030478@gmail.com>
2020-05-02 18:39:40 -0500

    update chart description and add link to repo

--- src/components/PositivityRateChart.vue
+++ src/components/PositivityRateChart.vue
@@ -6,17 +6,17 @@
         href="https://www.theatlantic.com/technology/archive/2020/04/us-coronavirus-outbreak-out-control-test-positivity-rate/610132/"
       >this article</a> in the Atlantic
       explaining why the positivity rate was a useful data. I looked for graphs that showed it's evolution but could not find any. There might be some, I just did not see it.
-      I used
-    </p>
-    <p>
-      <a
-        href="https://covidtracking.com/data/state/illinois#historical"
-      >covidtracking.com</a> for the US states data and
-      <a href="https://covid.ourworldindata.org/data">ourworldindata.org</a> for the countries data. To see how the data is teched checkout
-      <a href="https://github.com/vhetet/vuejs-testing-rate-data">this repo</a>.
     </p>
     <p>Not all countries/states report their cases so some charts are empty or are missing some data points.</p>
-      
+    <p>
+      I used
+      <a href="https://covidtracking.com/data/state/illinois#historical">covidtracking.com</a> for the US states data and
+      <a href="https://covid.ourworldindata.org/data">ourworldindata.org</a> for the countries data. To see how the data is stored checkout this
+      <a
+        href="https://github.com/vhetet/vuejs-testing-rate-data"
+      >repo</a>.
+      If you want to look at the code for the web app check this <a href="https://github.com/vhetet/vuejs-chartjs-testing-rate">repo</a>
+    </p>
 
     <select v-model="worldSelected">
       <option value="us">US states</option>
